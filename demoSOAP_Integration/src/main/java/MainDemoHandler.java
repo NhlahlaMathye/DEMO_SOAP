daf2809af89afcd3335b5b27e1e5e7ede5750d9c
NhlahlaMathye <Nhlahla.Mathye@hellogroup.co.za>
2022-05-17 01:07:04 +0200

    new project

--- demoSOAP_Integration/src/main/java/MainDemoHandler.java
+++ demoSOAP_Integration/src/main/java/MainDemoHandler.java
@@ -1,4 +1,5 @@
 import config.DemoUtils;
+import config.Utilities;
 import wcs.ListCountryNamesByNamesResponse;
 import wcs.TCountryName;
 import wss.ListOfLanguagesByNameResponse;
@@ -10,8 +11,7 @@ import java.util.logging.Logger;
 
 public class MainDemoHandler {
 
-    private static final String BASE_URL = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso";
-    private static final String MEDIA_TYPE = "text/xml; charset=utf-8";
+
     static boolean checkInfo;
     static int programLevel = 0;
 
@@ -45,12 +45,12 @@ public class MainDemoHandler {
                     programLevel = sc.nextInt();
                     break;
                 case 1:
-                    MainDemoHandler.checkListOfLanguages();
+                    Utilities.checkListOfLanguages();
                     programLevel = 0;
                     break;
 
                 case 2:
-                    MainDemoHandler.checkListOfCountryAndName();
+                    Utilities.checkListOfCountryAndName();
                     programLevel = 0;
                     break;
 
@@ -64,55 +64,5 @@ public class MainDemoHandler {
 
     }
 
-    public static void checkListOfCountryAndName()
-    {
-        TCountryName listOfCountryName = new TCountryName();
 
-        String objectXmlString = DemoUtils.marshallObject(listOfCountryName);
-        String objectSoapString = new DemoUtils().createGenericSoapString(objectXmlString);
-        String apiCallResponse = DemoUtils.apiRequest(objectSoapString, BASE_URL, MEDIA_TYPE );
-
-        if(apiCallResponse == null)
-        {
-            System.out.println("API Response call is null " + objectSoapString + " " + apiCallResponse );
-        }
-
-        ListCountryNamesByNamesResponse listCountries = (ListCountryNamesByNamesResponse) DemoUtils.unMarshallObject(apiCallResponse, ListCountryNamesByNamesResponse.class);
-
-        assert listCountries != null;
-        for (int i = 0; i < listCountries.getListCountryNamesByNamesResult().gettCountryNames().size(); i++)
-        {
-            String countyName = listCountries.getListCountryNamesByNamesResult().gettCountryNames().get(i).getName();
-            String countryCode = listCountries.getListCountryNamesByNamesResult().gettCountryNames().get(i).getIsoCOde();
-
-            System.out.println(" Country Name: " + countyName + " \n Country Code: " + countryCode + " \n");
-        }
-    }
-
-    public static void checkListOfLanguages()
-    {
-        TLanguage listOfLanguagesByName = new TLanguage();
-
-        String objectXmlString = DemoUtils.marshallObject(listOfLanguagesByName);
-        String objectSoapString = new DemoUtils().createGenericSoapString(objectXmlString);
-        String apiCallResponse = DemoUtils.apiRequest(objectSoapString, BASE_URL, MEDIA_TYPE );
-
-        //logger.info("Here is the response " + apiCallResponse);
-        if(apiCallResponse == null)
-        {
-            System.out.println("API Response call is null " + objectSoapString + " " + apiCallResponse );
-        }
-        ListOfLanguagesByNameResponse listLanguages = (ListOfLanguagesByNameResponse) DemoUtils.unMarshallObject(apiCallResponse, ListOfLanguagesByNameResponse.class);
-
-        assert listLanguages != null;
-        //System.out.println(listLanguages.getListOfLanguagesByNameResult().getTLanguages().get(0).getName());
-
-        for(int x = 0; x < listLanguages.getListOfLanguagesByNameResult().getTLanguages().size(); x++)
-        {
-            String name = listLanguages.getListOfLanguagesByNameResult().getTLanguages().get(x).getName();
-            String code = listLanguages.getListOfLanguagesByNameResult().getTLanguages().get(x).getIsoCode();
-
-            System.out.println(" Language name :" + name + "\n Language Code :" + code + "\n");
-        }
-    }
 }
---
+++ demoSOAP_Integration/src/main/java/config/Utilities.java
@@ -0,0 +1,64 @@
+package config;
+
+import wcs.ListCountryNamesByNamesResponse;
+import wcs.TCountryName;
+import wss.ListOfLanguagesByNameResponse;
+import wss.TLanguage;
+
+public class Utilities {
+
+    private static final String BASE_URL = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso";
+    private static final String MEDIA_TYPE = "text/xml; charset=utf-8";
+
+    public static void checkListOfCountryAndName()
+    {
+        TCountryName listOfCountryName = new TCountryName();
+
+        String objectXmlString = DemoUtils.marshallObject(listOfCountryName);
+        String objectSoapString = new DemoUtils().createGenericSoapString(objectXmlString);
+        String apiCallResponse = DemoUtils.apiRequest(objectSoapString, BASE_URL, MEDIA_TYPE );
+
+        if(apiCallResponse == null)
+        {
+            System.out.println("API Response call is null " + objectSoapString + " " + apiCallResponse );
+        }
+
+        ListCountryNamesByNamesResponse listCountries = (ListCountryNamesByNamesResponse) DemoUtils.unMarshallObject(apiCallResponse, ListCountryNamesByNamesResponse.class);
+
+        assert listCountries != null;
+        for (int i = 0; i < listCountries.getListCountryNamesByNamesResult().gettCountryNames().size(); i++)
+        {
+            String countyName = listCountries.getListCountryNamesByNamesResult().gettCountryNames().get(i).getName();
+            String countryCode = listCountries.getListCountryNamesByNamesResult().gettCountryNames().get(i).getIsoCOde();
+
+            System.out.println(" Country Name: " + countyName + " \n Country Code: " + countryCode + " \n");
+        }
+    }
+
+    public static void checkListOfLanguages()
+    {
+        TLanguage listOfLanguagesByName = new TLanguage();
+
+        String objectXmlString = DemoUtils.marshallObject(listOfLanguagesByName);
+        String objectSoapString = new DemoUtils().createGenericSoapString(objectXmlString);
+        String apiCallResponse = DemoUtils.apiRequest(objectSoapString, BASE_URL, MEDIA_TYPE );
+
+        //logger.info("Here is the response " + apiCallResponse);
+        if(apiCallResponse == null)
+        {
+            System.out.println("API Response call is null " + objectSoapString + " " + apiCallResponse );
+        }
+        ListOfLanguagesByNameResponse listLanguages = (ListOfLanguagesByNameResponse) DemoUtils.unMarshallObject(apiCallResponse, ListOfLanguagesByNameResponse.class);
+
+        assert listLanguages != null;
+        //System.out.println(listLanguages.getListOfLanguagesByNameResult().getTLanguages().get(0).getName());
+
+        for(int x = 0; x < listLanguages.getListOfLanguagesByNameResult().getTLanguages().size(); x++)
+        {
+            String name = listLanguages.getListOfLanguagesByNameResult().getTLanguages().get(x).getName();
+            String code = listLanguages.getListOfLanguagesByNameResult().getTLanguages().get(x).getIsoCode();
+
+            System.out.println(" Language name :" + name + "\n Language Code :" + code + "\n");
+        }
+    }
+}
